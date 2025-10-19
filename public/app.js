3747718cd1ca5651a698c4353d60a81e14eac0e8
Pietro Rossi <LP4PRossi@gibwintra.ch>
2025-10-19 16:04:58 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- IMPORTANT ---
     // This should already be your Google Cloud Run URL
-    const SIGNALING_SERVER_URL = 'https://live-poll-server-147708164583.us-central1.run.app/'; // PASTE YOUR URL HERE
+    const SIGNALING_SERVER_URL = 'wss://live-poll-server-147708164583.us-central1.run.app/'; // PASTE YOUR URL HERE
 
     // --- UI Elements ---
     const roleSelection = document.getElementById('role-selection');
